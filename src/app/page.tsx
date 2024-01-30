fc144a5129124eee6623b44ee3e86762155033b5
Victorio Caccioppoli <114512180+Victorioo@users.noreply.github.com>
2024-01-30 03:21:01 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,120 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header className="h-14 bg-gray-800 flex items-center justify-between w-full border-b-2 border-[#2beba3]">
+        <div className="w-full flex justify-between max-w-[70%] mx-auto items-center">
+          <h1>
+            <span className="font-bold">ARRO</span> STORES
+          </h1>
+          <ul className="flex gap-10 list-none font-bold text-md">
+            <a href="#tiendas">Tiendas</a>
+            <a href="#nosotros">Nosotros</a>
+          </ul>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      </header>
+      <main className="hero relative h-[calc(100vh-3.5rem)] bg-gray-900">
+        <section className="z-20 w-full h-full flex items-center justify-between mx-auto max-w-[70%]">
+          <article className="max-w-[50%] flex flex-col items-start gap-4 sm:max-w-[100%]">
+            <h1 className="text-4xl leading-tight font-bold max-w-[29ch]">
+              Conocé nuestras tiendas de ropa, y vestí siempre genial.
+            </h1>
+            <button className="bg-[#2beba3] px-10 py-2 font-bold text-black text-xl rounded-xl">
+              VER TIENDAS
+            </button>
+            <p className="font-bold text-xl max-w-[40ch] my-4 sm:text-md">
+              HACÉ CLICK EN EL BOTÓN DE ARRIBA Y HACÉ QUE TU LOOK SE VEA GENIAL
+            </p>
+          </article>
+          <Image
+            src={"/stores.jpg"}
+            width={100}
+            height={50}
+            className="w-[500px] h-[500px] object-cover rounded-full levitate sm:hidden lg:hidden"
+            alt=""
+            unoptimized={true}
+          ></Image>
+        </section>
+        <section
+          className="grid grid-cols-2 grid-rows-1 items-center h-96 overflow-hidden"
+          id="tiendas"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <a
+            className="rola-store bg-gray-200 flex items-center justify-center flex-col gap-4 p-10 h-full text-black text-center"
+            href="https://www.instagram.com/rolastoreok/"
+            target="_blank"
+          ></a>
+          <a
+            className="vitto-store bg-black w-full flex items-center justify-center flex-col gap-4 p-10 h-full text-center"
+            href="https://www.instagram.com/vittostore.ok/"
+            target="_blank"
+          ></a>
+        </section>
+        <section className="bg-gray-900 flex items-start justify-center py-24">
+          <div className="flex flex-col items-center justify-center max-w-[50%] gap-5 sm:max-w-[90%]">
+            <h1 className="text-2xl font-semibold text-white">
+              Sobre nosotros
+            </h1>
+            <p className="leading-loose text-lg text-center text-gray-400 max-w-[80%]">
+              Somos un grupo de mujeres que trabajan para mejorar cada día
+              nuestro producto, trayendo siempre la mejor calidad para las
+              personas que residen en Arroyito, empezamos haciendo de{" "}
+              <a
+                href="https://www.instagram.com/rolastoreok/"
+                className="text-[#2beba3] hover:text-[#2beba5b0]"
+                target="_blank"
+              >
+                Rola Store
+              </a>{" "}
+              nuestra tienda para venta de vestimenta de mujer, y luego
+              decidimos que los hombres también debían disfrutar de nuestra
+              ropa, por lo que creamos{" "}
+              <a
+                href="https://www.instagram.com/vittostore.ok/"
+                className="text-[#2beba3] hover:text-[#2beba5b0]"
+                target="_blank"
+              >
+                Vitto Store
+              </a>
+              .
+            </p>
+          </div>
+        </section>
+        <section className="flex items-center justify-center flex-col p-10 bg-gray-900 gap-8">
+          <h1 className="text-2xl font-semibold">
+            Contamos con el apoyo de...
+          </h1>
+          <article className="grid grid-cols-3 grid-rows-1 gap-4">
+            <Image
+              src={"/bohe-store.jpg"}
+              width={150}
+              height={150}
+              alt="Foto de marca"
+              className="rounded-full opacity-70"
+            ></Image>
+            <Image
+              src={"/fca.jpg"}
+              width={150}
+              height={150}
+              alt="Foto de marca"
+              className="rounded-full opacity-70"
+            ></Image>
+            <Image
+              src={"/la-konga.jpg"}
+              width={150}
+              height={150}
+              alt="Foto de marca"
+              className="rounded-full opacity-70"
+            ></Image>
+          </article>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        <footer className="bg-gray-950 flex items-center justify-center p-14">
+          <p className="max-w-[50%] text-center text-gray-500 sm:max-w-[90%]">
+            © 2024 ARRO STORE Todos los derechos reservados.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </footer>
+      </main>
+    </>
   );
 }
